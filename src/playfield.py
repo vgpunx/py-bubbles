35ddc13b73dbd2eb50c89e2aa7da5b9448d7918b
Bob Taylor <12554820+taylor-madeak@users.noreply.github.com>
2018-02-14 21:22:22 -0900

    Begin develop bubble collision

--- src/playfield.py
+++ src/playfield.py
@@ -34,8 +34,22 @@ class Playfield:
         pygame.draw.rect(self.surface, pygame.Color("WHITE"), self.surface.get_rect(), 5)
 
         try:
+            if len(self.active_bubbles) > 0:
+                fired_bubble = self.active_bubbles.sprites()[0]
+            else:
+                fired_bubble = None
+
             for bubble in chain(self.bubble_map, self.active_bubbles):
                 bubble.update()
+
+                if fired_bubble:
+                    if pygame.sprite.collide_circle(fired_bubble, bubble):
+                        #fired_bubble.set_velocity(0)
+                        # this code doesn't work
+                        print("collision with bubble at {0}".format(bubble.pos))
+                        self.bubble_map.add(fired_bubble)
+                        self.active_bubbles.remove(fired_bubble)
+
                 self.surface.blit(bubble.image, bubble.rect)
         except:
             raise
