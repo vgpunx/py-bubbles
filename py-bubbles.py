d0922157e8a8e53c12ed1b932ccf8492417338fc
Bob Taylor <12554820+taylor-madeak@users.noreply.github.com>
2018-02-14 18:50:01 -0900

    Truing up the ignored files.

--- py-bubbles.py
+++ py-bubbles.py
@@ -12,15 +12,6 @@ def main():
     PFLD_SIZE = (DISP_SIZE[0] * 0.65, DISP_SIZE[1] * 0.98)  # 65% scr width, 85% scr height
     CELL_SIZE = (PFLD_SIZE[0] / 23, PFLD_SIZE[0] / 23)  # Fit 15 bubbles across
 
-    ## PATHS
-    BGM_PATH = os.path.join(os.curdir, 'resource', 'audio', 'bgm')
-    SFX_PATH = os.path.join(os.curdir, 'resource', 'audio', 'sfx')
-    BGI_PATH = os.path.join(os.curdir, 'resource', 'image', 'bkg')
-    SPR_PATH = os.path.join(os.curdir, 'resource', 'image', 'sprites')
-
-    ## BUBBLE MAP
-
-
     ## COLORS
 
     ## ALIGNMENT
