32bfb10c3fc21df4b9711f1b1ac51975c84afc84
Bob Taylor <12554820+taylor-madeak@users.noreply.github.com>
2018-02-14 18:50:01 -0900

    Truing up the ignored files.

--- py-bubbles.py
+++ py-bubbles.py
@@ -11,9 +11,6 @@ def main():
     PFLD_SIZE = (DISP_SIZE[0] * 0.65, DISP_SIZE[1] * 0.98)  # 65% scr width, 85% scr height
     CELL_SIZE = (PFLD_SIZE[0] / 23, PFLD_SIZE[0] / 23)  # Fit 15 bubbles across
 
-    ## BUBBLE MAP
-
-
     ## COLORS
 
     ## ALIGNMENT
