37acbd3669fdffdf58c8ea894051fdc31628c0fb
Bob Taylor <12554820+taylor-madeak@users.noreply.github.com>
2018-02-21 17:09:04 -0900

    Crete constants.py for storing and using constants between modules.

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+import os
+
+__all__ = ["DISP_SIZE", "PFLD_SIZE", "CELL_SIZE", "BGM_PATH", "SFX_PATH", "BGI_PATH", "SPR_PATH"]
+
+DISP_SIZE = (800, 600)
+PFLD_SIZE = (DISP_SIZE[0] * 0.65, DISP_SIZE[1] * 0.98)  # 65% scr width, 85% scr height
+CELL_SIZE = (PFLD_SIZE[0] / 23, PFLD_SIZE[0] / 23)  # Fit 15 bubbles across
+BGM_PATH = os.path.join(os.curdir, 'resource', 'audio', 'bgm')
+SFX_PATH = os.path.join(os.curdir, 'resource', 'audio', 'sfx')
+BGI_PATH = os.path.join(os.curdir, 'resource', 'image', 'bg')
+SPR_PATH = os.path.join(os.curdir, 'resource', 'image', 'sprites')
