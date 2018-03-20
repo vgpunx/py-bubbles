2fcd2e105d3e17ea7667d4b7fe39d3375745ce33
Bob Taylor <12554820+taylor-madeak@users.noreply.github.com>
2018-03-20 15:53:45 -0800

    Code cleanup.

--- py-bubbles.py
+++ py-bubbles.py
@@ -6,26 +6,11 @@ from pygame.locals import *
 
 
 def main():
-    ### CONSTANTS ###
-
-    ## SIZE
-    # DISP_SIZE = (800, 600)
-    # PFLD_SIZE = (DISP_SIZE[0] * 0.65, DISP_SIZE[1] * 0.98)  # 65% scr width, 85% scr height
-    # CELL_SIZE = (PFLD_SIZE[0] / 23, PFLD_SIZE[0] / 23)  # Fit 15 bubbles across
-
-    ## COLORS
-
-    ## PATHS
-    # BGM_PATH = os.path.join(os.curdir, 'resource', 'audio', 'bgm')
-    # SFX_PATH = os.path.join(os.curdir, 'resource', 'audio', 'sfx')
-    # BGI_PATH = os.path.join(os.curdir, 'resource', 'image', 'bg')
-    # SPR_PATH = os.path.join(os.curdir, 'resource', 'image', 'sprites')
 
     # initialize pygame
     pygame.init()
 
     # set up the main window
-
     screen = pygame.display.set_mode(DISP_SIZE)
     pygame.display.set_caption('Py-Bubbles')
     screen.set_colorkey(pygame.Color('MAGENTA'))
