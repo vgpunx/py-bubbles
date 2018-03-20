64f51f0b776a5d0bd04ec430121a35d0e3fb733a
Bob Taylor <repairman@taylor-madeak.org>
2018-03-19 19:12:26 -0800

    Refactor into subclass of ConfigParser

--- src/settings.py
+++ src/settings.py
@@ -1,29 +1,26 @@
-from configparser import ConfigParser
-import os, sys, errno
+import configparser
+import os, errno
 
 
-class Settings:
-    def __init__(self, filepath) -> None:
-        # instantiate parser
-        self.config = ConfigParser()
-        self.load(filepath)
-
+class Settings(configparser.ConfigParser):
+    def __init__(self, filepath):
+        configparser.ConfigParser.__init__(self)
         # save the conf file path
         self.configFilePath = filepath
 
     def load(self):
-        if not os.path.exist(self.configFilePath):
+        if not os.path.exists(self.configFilePath):
             raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), self.configFilePath)
 
-        self.config.read(self.configFilePath)
+        self.read(self.configFilePath)
 
     def save(self):
         with open(self.configFilePath, 'w') as fp:
-            self.config.write(fp)
+            self.write(fp)
 
     def writeDefaults(self):
         # clear configparser contents
-        self.config.clear()
+        self.clear()
 
         # this dict will become our default config file
         conf = {
@@ -39,7 +36,7 @@ class Settings:
         }
 
         # load settings dict into parser
-        self.config.read_dict(conf)
+        self.read_dict(conf)
 
         # decide if we should make some noise
         if not self.configFilePath:
@@ -49,4 +46,4 @@ class Settings:
 
         # write it out
         with open(self.configFilePath, 'w') as fp:
-            self.config.write(fp)
+            self.write(fp)
